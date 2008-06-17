57019e77532919d585d243e8bbdb977cec437053
Zack Chandler <zackchandler@depixelate.com>
2008-06-17 14:43:57 -0700

    EsimateLineGroups

---
+++ model_maps/EstimateLineGroup.rb
@@ -0,0 +1,17 @@
+# auto-generated from json definitions
+module QBXML
+  module ModelMaps
+    EstimateLineGroup = {
+      :txn_line_id => 'TxnLineID',
+      :item_group_ref_list_id => 'ItemGroupRef/ListID',
+      :item_group_ref_full_name => 'ItemGroupRef/FullName',
+      :desc => 'Desc',
+      :quantity => 'Quantity',
+      :unit_of_measure => 'UnitOfMeasure',
+      :override_uom_set_ref_list_id => 'OverrideUOMSetRef/ListID',
+      :override_uom_set_ref_full_name => 'OverrideUOMSetRef/FullName',
+      :is_print_items_in_group => 'IsPrintItemsInGroup',
+      :total_amount => 'TotalAmount'
+    }
+  end
+end
